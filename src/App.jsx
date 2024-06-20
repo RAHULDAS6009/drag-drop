d0135686e9206464d3ec9c33a8b4c198a5d6ba46
Rahul Das <das206053@gmail.com>
2024-06-20 16:16:15 +0530

    toggle-mode-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
 import { Task } from "./components/Task";
+import darkMode from "./assets/dark.svg";
+import whiteMode from "./assets/whiteDarkicon.svg";
 let globalId = 0;
 
 function App() {
@@ -9,15 +11,43 @@ function App() {
     { id: globalId++, title: "gym" },
     { id: globalId++, title: "reading" },
   ]);
+  const [mode, setMode] = useState(false);
 
   function addTask() {
+    if (title === "" || todoList.length == 9) {
+      return;
+    }
     setTodoList((prvlist) => [...prvlist, { id: globalId++, title }]); //previous list +new object
     setTitle("");
   }
 
+
+
   return (
-    <div className="w-full h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%  grid place-items-center">
-      <div className=" w-[600px] bg-green-500 h-[500px]  p-4 rounded-xl ring">
+    <div className="w-full h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% dark:bg-gradient-to-r from-sky-500from-10% via-indigo-500via-30% to-emerald-200to-90% grid place-items-center">
+      <button onClick={()=>{
+        if(mode == true){
+          setMode(false);
+        }else{
+          setMode(true);
+        }
+      }} className="w-5 h-5">
+        {mode ? (
+          <img src={whiteMode} alt="white" />
+        ) : (
+          <img src={darkMode} alt="darkMode" />
+        )}
+      </button>
+      <div
+        style={{
+          height: `${
+            todoList.length <= 4
+              ? "500"
+              : `${500 + (todoList.length * 20 + 100)}`
+          }px`,
+        }}
+        className=" w-[600px] bg-green-500   p-4 rounded-xl ring"
+      >
         <div className="text-white text-[65px] mb-10 font-mono font-bold text-center">
           TasKify
         </div>
@@ -29,6 +59,7 @@ function App() {
             onChange={(e) => {
               setTitle(e.target.value);
             }}
+            value={title}
           />
           <button
             className="rounded-full bg-indigo-400 focus:bg-blue-400 w-10 h-10  text-white font-medium absolute right-0 mr-1.2 mt-[2px] border ring ring-blue-100"
@@ -50,17 +81,20 @@ function App() {
 
           <div className="w-full ml-4">
             <CompletedTask />
-            <Task/>
+            <Task />
           </div>
         </div>
-
       </div>
     </div>
   );
 }
 
 function ActiveTask() {
-  return <div className="bg-indigo-400 rounded-lg border text-white h-10 p-2 font-bold ">Active Task</div>;
+  return (
+    <div className="bg-indigo-400 rounded-lg border text-white h-10 p-2 font-bold ">
+      Active Task
+    </div>
+  );
 }
 
 function CompletedTask() {
